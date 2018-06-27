0c2149e126e782a41249495858ff7df3b8559230
Lazaro Henrique <lazaro.hcm@gmail.com>
2018-06-27 11:56:04 -0300

    feat: Testing api routes. Creation of controller and service methods

---
+++ src/ApiRestBundle/Controller/ProductController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: unasus
+ * Date: 6/27/18
+ * Time: 10:00 AM
+ */
+
+namespace ApiRestBundle\Controller;
+
+
+class ProductController
+{
+
+}
---
+++ tests/ApiRestBundle/Controller/ProductControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: unasus
+ * Date: 6/27/18
+ * Time: 10:08 AM
+ */
+
+namespace Tests\ApiRestBundle\Controller;
+
+
+class ProductControllerTest
+{
+
+}
