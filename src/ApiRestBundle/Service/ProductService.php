4283cf818384561ee78f52b96f5b869aeea45236
Lazaro Henrique <lazaro.hcm@gmail.com>
2018-06-27 09:45:55 -0300

    feat: Creating product service

---
+++ src/ApiRestBundle/Service/ProductService.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: unasus
+ * Date: 6/27/18
+ * Time: 9:17 AM
+ */
+
+namespace ApiRestBundle\Service;
+
+
+use ApiRestBundle\Service\Util\BaseService;
+use AppBundle\Entity\TbProduct;
+use AppBundle\Form\TbProductType;
+use Symfony\Component\HttpFoundation\Request;
+
+class ProductService extends BaseService
+{
+    public function findById($id) {
+        $product = $this->repositorio->find($id);
+
+        if(!$product) {
+            throw new \Exception('Nothing found!');
+        }
+        return $product;
+    }
+
+    public function seek() {
+        $products = $this->repositorio->findAll();
+
+        return $products;
+    }
+
+    public function create(Request $request){
+        $attributes = $request->get('product');
+
+        $product = new TbProduct();
+        $form = $this->createForm($product);
+        $form->submit($attributes);
+
+        if(!$form->isValid()) {
+            throw new \Exception($form->getErrors());
+        }
+
+        return $this->save($product);
+    }
+
+    public function edit(Request $request) {
+        $attributes = $request-get('product');
+
+        if(!isset($attributes['id'])) {
+            throw new \Exception('THe provided is invalid');
+        }
+
+        $product = $this->findById($attributes['id']);
+
+        $form = $this->creteForm($product);
+        $form->submit($attributes);
+
+        if(!$form->isValid()) {
+            throw new \Exception($form->getErros());
+        }
+
+        return $this->save($product);
+    }
+
+
+    public function save(TbProduct $product) {
+        $this->em->persist($product);
+        $this->em->flush();
+
+        return $product;
+    }
+
+    public function createForm(TbProduct $entity = null) {
+        $product = $entity == null ? new  TbProduct() : $entity;
+
+        $form = $this->formFactory->create(TbProductType::class, $product);
+        return $form;
+    }
+
+
+}
